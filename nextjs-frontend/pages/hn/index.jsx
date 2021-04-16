33f1c0e590bb74e4e2aed35a97da267d8b7f7c5a
fabio.carvalho <fabio.carvalho@mog-technologies.com>
2021-04-16 15:01:35 +0100

    better code structure + hackernews dynamic routes

---
+++ nextjs-frontend/pages/hn/index.jsx
@@ -0,0 +1,64 @@
+import Link from 'next/link';
+import styles from '../../styles/Home.module.css';
+import { useRouter } from 'next/router';
+
+const Stories = ({ topStories }) => {
+  const { pathname } = useRouter();
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Hack News - Also The Best API ever</h1>
+        <h2>
+          <Link href='/'>
+            <a>Back to Homepage</a>
+          </Link>
+        </h2>
+        <div>
+          <h3 className={styles.text}>Top 5 Stories</h3>
+          <div>
+            {topStories.map((story) => (
+              <Link key={story.id} href={`${pathname}/${story.id}`}>
+                <a>
+                  <p className={styles.text}>
+                    {story.title}{' '}
+                    <label className={styles.author}>by: {story.by}</label>
+                  </p>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  const api = 'https://hacker-news.firebaseio.com/v0';
+
+  async function fetchTopStoriesIds() {
+    const res = await fetch(`${api}/topstories.json`);
+    const allTopStoriesIds = await res.json();
+    // Get the top 5 ids from the response
+    return allTopStoriesIds.slice(0, 5);
+  }
+
+  async function fetchTopStories() {
+    const topStoriesIds = await fetchTopStoriesIds();
+    return await Promise.all(
+      topStoriesIds.map(async (storyId) => {
+        const res = await fetch(`${api}/item/${storyId}.json`);
+        return await res.json();
+      })
+    );
+  }
+
+  return {
+    props: {
+      topStories: await fetchTopStories(),
+    },
+  };
+};
+
+export default Stories;
--- nextjs-frontend/pages/index.jsx
+++ nextjs-frontend/pages/index.jsx
@@ -1,28 +1,26 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome {' '}
-          <Link href="/posts/quotes">
+          Welcome{' '}
+          <Link href='/kanye'>
             <a>to Kanye Rest!</a>
           </Link>
           <br />
-          <Link href="/posts/hacknews">
+          <Link href='/hn'>
             <a>and to HackerNews API!</a>
           </Link>
         </h1>
       </main>
 
       <footer className={styles.footer}>
-        <a>
-          Powered by{' '} Fábio Carvalho
-        </a>
+        <a>Powered by Fábio Carvalho</a>
       </footer>
     </div>
-  )
+  );
 }
