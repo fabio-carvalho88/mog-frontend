28339e193678f9b72ce9203d26219cbfeebcd766
mvalente <matilde.valente@mog-technologies.com>
2021-05-18 17:58:48 +0100

    Added Card Item component for products section

--- nextjs-frontend/components/Cards/CardItem.jsx
+++ nextjs-frontend/components/Cards/CardItem.jsx
@@ -1,19 +1,14 @@
-import {Card, Col, Row} from "antd";
-import {useRouter} from "next/router";
-import styles from "../../styles/Home.module.css";
-import Link from "next/link";
+import {Card} from "antd";
+import styles from "./CardItem.module.css";
 
-const CardItem = () => {
+const CardItem = (props) => {
   return (
-    <Card>
-      <h4>OTT-TV Platform</h4>
-      <p>
-        With Vizzi is easy to launch your all-in-one video platform, from
-        acquisition to distribution, with no loose ends. Start monetizing your
-        content, today.
-      </p>
-      <a href="">Read More</a>
-    </Card>
+    <div className={styles.card}>
+      <div>{props.icon}</div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <a href={props.explore}>Explore</a>
+    </div>
   );
 };
 export default CardItem;
