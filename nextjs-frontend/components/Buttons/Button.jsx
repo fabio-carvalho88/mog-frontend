ad01575456ffa6f9a7704891d5a48e30fe1ffbce
mvalente <matilde.valente@mog-technologies.com>
2021-05-28 09:27:18 +0100

    Added inline style to button so that the color/border can be costumized accordingly to the background color of the section

--- nextjs-frontend/components/Buttons/Button.jsx
+++ nextjs-frontend/components/Buttons/Button.jsx
@@ -1,9 +1,33 @@
-import { RightOutlined } from "@ant-design/icons";
+import {RightOutlined} from "@ant-design/icons";
 import {Button} from "antd";
 import styles from "./Button.module.css";
 
 const Btn = (props) => {
-  return <Button className={styles.btn}>{props.children}<RightOutlined className={styles.icon}/></Button>;
+  return (
+    <Button
+      ghost
+      className={styles.btn}
+      style={{border: `1px solid ${props.border}`, color: `${props.color}`}}
+    >
+      {props.children}
+      <svg
+        width="7"
+        height="13"
+        viewBox="0 0 7 13"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{marginLeft: "16px"}}
+      >
+        <path
+          d="M1 12L6 6.5L1 1"
+          stroke={props.btnIconColor}
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </Button>
+  );
 };
 
 export default Btn;
